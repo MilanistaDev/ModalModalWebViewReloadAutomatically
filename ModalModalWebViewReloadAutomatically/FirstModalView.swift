eb69fe627d5536ac5866c10d1e2c31df39450d58
Takuya Aso <milanista224@gmail.com>
2025-04-15 11:21:41 +0900

    Implemented ModalFirstView.

--- ModalModalWebViewReloadAutomatically/FirstModalView.swift
+++ ModalModalWebViewReloadAutomatically/FirstModalView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct FirstModalView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var isPresented = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                Button {
+                    isPresented = true
+                } label: {
+                    Text("Present WebView")
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.top, 120)
+
+                Spacer()
+            }
+            .navigationTitle("First Modal View")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Close")
+                    }
+                }
+            }
+        }
+        .fullScreenCover(isPresented: $isPresented) {
+            WebView()
+        }
     }
 }
 
---
+++ ModalModalWebViewReloadAutomatically/WebView.swift
@@ -0,0 +1,18 @@
+//
+//  WebView.swift
+//  ModalModalWebViewReloadAutomatically
+//
+//  Created by Takuya Aso on 2025/04/15.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebView()
+}
